77412c386e1115f5da184ade3a506ba043d1feac
Anthony Decena <anthony@mindelusions.com>
2012-03-19 19:32:25 -0700

    All mashup examples added and working.

--- Resources/ui/common/mashups/facebook.js
+++ Resources/ui/common/mashups/facebook.js
@@ -1,36 +1,34 @@
+function facebook(_args) {
+	var self = Ti.UI.createWindow({
+		backgroundColor:'#fff'
+	});
+	//create table view data object
+	var data = [
+		{title:'Login/Logout', hasChild:true, test:'ui/common/mashups/facebook_login_logout'},
+		{title:'Query', hasChild:true, test:'ui/common/mashups/facebook_query'},
+		{title:'Properties', hasChild:true, test:'ui/common/mashups/facebook_properties'},
+		{title:'Publish Stream', hasChild:true, test:'ui/common/mashups/facebook_publish_stream'},
+		{title:'Photos', hasChild:true, test:'ui/common/mashups/facebook_photos'}
+	
+	];
+	
+	// create table view
+	var tableview = Titanium.UI.createTableView({
+		data:data
+	});
+	
+	// create table view event listener
+	tableview.addEventListener('click', function(e) {
+		if (e.rowData.test) {
+			var ExampleWindow = require(e.rowData.test),
+				win = new ExampleWindow();
+			_args.containingTab.open(win,{animated:true});
+		}
+	});
+	
+	// add table view to the window
+	self.add(tableview);
+	return self;
+};
 
-
-//create table view data object
-var data = [
-	{title:'Login/Logout', hasChild:true, test:'../examples/facebook_login_logout.js'},
-	{title:'Query', hasChild:true, test:'../examples/facebook_query.js'},
-	{title:'Properties', hasChild:true, test:'../examples/facebook_properties.js'},
-	{title:'Publish Stream', hasChild:true, test:'../examples/facebook_publish_stream.js'},
-	{title:'Photos', hasChild:true, test:'../examples/facebook_photos.js'}
-
-];
-
-
-// create table view
-var tableview = Titanium.UI.createTableView({
-	data:data
-});
-
-// create table view event listener
-tableview.addEventListener('click', function(e)
-{
-	if (e.rowData.test)
-	{
-		var win = Titanium.UI.createWindow({
-			url:e.rowData.test,
-			title:e.rowData.title
-		});
-		Titanium.UI.currentTab.open(win,{animated:true});
-	}
-});
-
-// add table view to the window
-Titanium.UI.currentWindow.add(tableview);
-
-
-
+module.exports = facebook;
