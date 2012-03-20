7a70aa3a1fddccc5f3854a517d662099d420a007
Anthony Decena <anthony@mindelusions.com>
2012-03-20 16:09:30 -0700

    Phone > Camera examples and all image resources

---
+++ Resources/ui/common/phone/camera_basic.js
@@ -0,0 +1,53 @@
+function cam_basic() {
+	var win = Titanium.UI.createWindow();
+	Titanium.Media.showCamera({
+	
+		success:function(event)
+		{
+			var cropRect = event.cropRect;
+			var image = event.media;
+	
+			Ti.API.debug('Our type was: '+event.mediaType);
+			if(event.mediaType == Ti.Media.MEDIA_TYPE_PHOTO)
+			{
+				var imageView = Ti.UI.createImageView({
+					width:win.width,
+					height:win.height,
+					image:event.media
+				});
+				win.add(imageView);
+			}
+			else
+			{
+				alert("got the wrong type back ="+event.mediaType);
+			}
+		},
+		cancel:function()
+		{
+		},
+		error:function(error)
+		{
+			// create alert
+			var a = Titanium.UI.createAlertDialog({title:'Camera'});
+	
+			// set message
+			if (error.code == Titanium.Media.NO_CAMERA)
+			{
+				a.setMessage('Please run this test on device');
+			}
+			else
+			{
+				a.setMessage('Unexpected error: ' + error.code);
+			}
+	
+			// show alert
+			a.show();
+		},
+		saveToPhotoGallery:true,
+		allowEditing:true,
+		mediaTypes:[Ti.Media.MEDIA_TYPE_VIDEO,Ti.Media.MEDIA_TYPE_PHOTO]
+	});
+	return win;
+};
+
+module.exports = cam_basic;
