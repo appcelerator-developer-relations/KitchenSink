7704bfdbf8b3a6abb4c98a54c03f2aa9706c0d0f
Anthony Decena <anthony@mindelusions.com>
2012-03-20 10:17:16 -0700

    Platform > network, logging, commonjs, app events, and app data examples

---
+++ Resources/ui/common/platform/app_events.js
@@ -0,0 +1,38 @@
+function app_events() {
+	var win = Titanium.UI.createWindow();
+	
+	
+	var label = Ti.UI.createLabel({
+		text:'No app event received. Make call while running app',
+		textAlign:'center',
+		width:'auto'
+	});
+	
+	win.add(label);
+	
+	var paused = false;
+	
+	Titanium.App.addEventListener('pause',function(e)
+	{
+		paused = true;
+		label.text = "App has been paused";
+	});
+	
+	Titanium.App.addEventListener('resume',function(e)
+	{
+		if (paused)
+		{
+			label.text = "App has resumed";
+		}
+		else
+		{
+			label.text = "App has resumed (w/o pause)";
+		}
+	});
+	
+	return win;
+};
+
+module.exports = app_events;
+	
+
