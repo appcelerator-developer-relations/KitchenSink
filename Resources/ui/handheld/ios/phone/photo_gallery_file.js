1e329adbb00da0b7a100d2d882acbeddc827e799
Anthony Decena <anthony@mindelusions.com>
2012-03-20 16:45:01 -0700

    Phone > Screenshot and Photo Gallery examples.

---
+++ Resources/ui/handheld/ios/phone/photo_gallery_file.js
@@ -0,0 +1,22 @@
+function gallery_file() {
+	var win = Ti.UI.createWindow();
+	var f = Titanium.Filesystem.getFile(Titanium.Filesystem.resourcesDirectory,'images/appcelerator_small.png');
+	
+	Titanium.Media.saveToPhotoGallery(f,{
+		success: function(e) {
+			Titanium.UI.createAlertDialog({
+				title:'Photo Gallery',
+				message:'Check your photo gallery for an appcelerator logo'
+			}).show();		
+		},
+		error: function(e) {
+			Titanium.UI.createAlertDialog({
+				title:'Error saving',
+				message:e.error
+			}).show();
+		}
+	});
+	return win;
+};
+
+module.exports = gallery_file;
