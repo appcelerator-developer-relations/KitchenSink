46a3e296030e38ff6efe681354dc083f2c22bd10
Anthony Decena <anthony@mindelusions.com>
2012-03-21 14:31:41 -0700

    BaseUI > Views > All Common TableView examples

---
+++ Resources/ui/common/baseui/table_view_api_sections.js
@@ -0,0 +1,27 @@
+function tv_api_sections() {
+	var win = Ti.UI.createWindow();
+	
+	var section1 = Ti.UI.createTableViewSection({
+		headerTitle:'Header 1'
+	});
+	for (var i=0; i < 4; i++) {
+		section1.add(Ti.UI.createTableViewRow({
+			title:'Row '+i
+		}));
+	}
+	
+	var section2 = Ti.UI.createTableViewSection();
+	for (var i=4; i < 10; i++) {
+		section2.add(Ti.UI.createTableViewRow({
+			title:'Row '+i
+		}));
+	}
+	
+	var tv = Ti.UI.createTableView({
+		data:[section1,section2]
+	});
+	win.add(tv);
+	return win;
+};
+
+module.exports = tv_api_sections;
