3e4b10abc0d4efa063f467be94b51345b081433a
Anthony Decena <anthony@mindelusions.com>
2012-03-21 17:47:03 -0700

    BaseUI > Views > All TableView examples

---
+++ Resources/ui/handheld/ios/baseui/table_view_noscroll.js
@@ -0,0 +1,28 @@
+function tv_noscroll() {
+	var w = Ti.UI.createWindow();
+	var search = Titanium.UI.createSearchBar({
+		showCancel:false,
+		hintText:'type in me then scroll'
+	});
+	
+	var tv = Ti.UI.createTableView({
+		scrollable:false,
+		search:search
+	});
+	w.add(tv);
+	
+	var data = [];
+	var count = 5;
+	
+	for (var c=0;c<count;c++)
+	{
+		var row = Ti.UI.createTableViewRow({title:"Row "+(c+1)});
+		data[c] = row;
+	}
+	
+	tv.data = data;
+
+	return w;
+};
+
+module.exports = tv_noscroll;
---
+++ Resources/ui/handheld/ios/baseui/table_view_touch.js
@@ -0,0 +1,43 @@
+function tv_touch() {
+	var win = Ti.UI.createWindow();
+	// create table view data object
+	var data = [];
+	
+	for (var x=0;x<4;x++)
+	{
+		//var view = Ti.UI.createView();
+		var label = Ti.UI.createLabel({
+			text:'Row Label ' + x,
+			height:'auto',
+			width:'auto',
+			color:'#336699',
+			left:10
+		});
+		var row = Ti.UI.createTableViewRow({height:50});
+		//view.add(label);
+		row.add(label);
+		data.push(row);
+	}
+	
+	// create table view
+	var tableview = Titanium.UI.createTableView({
+		data:data
+	});
+	
+	tableview.addEventListener('touchstart', function(e)
+	{
+		e.row.children[0].color = '#fff';
+	});
+	
+	tableview.addEventListener('touchend', function(e)
+	{
+		e.row.children[0].color = '#336699';
+	});
+	
+	
+	// add table view to the window
+	win.add(tableview);
+	return win;
+};
+
+module.exports = tv_touch;
