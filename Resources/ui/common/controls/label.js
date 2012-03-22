a48d18a6a22355a497f80383c9dacfcae3309975
tonylukasavage <anthony.lukasavage@gmail.com>
2012-03-21 23:01:28 -0400

    Controls > Label

--- Resources/ui/common/controls/label.js
+++ Resources/ui/common/controls/label.js
@@ -1,31 +1,37 @@
-// create label view data object
-var data = [
-	{title:'Basic', hasChild:true, test:'../examples/label_basic.js'}
-];
-
-// add android specific tests
-if (Titanium.Platform.name == 'android')
-{
-	data.push({title:'Auto Link', hasChild:true, test:'../examples/label_linkify.js'});
+function label(_args) {
+	var self = Ti.UI.createWindow({
+		title:_args.title,
+		backgroundColor:'white'
+	});
+	
+	// create label view data object
+	var data = [
+		{title:'Basic', hasChild:true, test:'ui/common/controls/label_basic'}
+	];
+	
+	// add android specific tests
+	if (Titanium.Platform.name == 'android')
+	{
+		data.push({title:'Auto Link', hasChild:true, test:'ui/handheld/android/controls/label_linkify'});
+	}
+	
+	// create table view
+	var tableview = Titanium.UI.createTableView({
+		data:data
+	});
+	
+	// create table view event listener
+	tableview.addEventListener('click', function(e) {
+		if (e.rowData.test) {
+			var ExampleWindow = require(e.rowData.test),
+				win = new ExampleWindow(_args);
+			_args.containingTab.open(win,{animated:true});
+		}
+	});
+	
+	// add table view to the window
+	self.add(tableview);
+	return self;
 }
 
-// create table view
-var tableview = Titanium.UI.createTableView({
-	data:data
-});
-
-// create table view event listener
-tableview.addEventListener('click', function(e)
-{
-	if (e.rowData.test)
-	{
-		var win = Titanium.UI.createWindow({
-			url:e.rowData.test,
-			title:e.rowData.title
-		});
-		Titanium.UI.currentTab.open(win,{animated:true});
-	}
-});
-
-// add table view to the window
-Titanium.UI.currentWindow.add(tableview);
+module.exports = label;
