1671c704f74f3145bd90fa0e811fe44885a02fc8
Anthony Decena <anthony@mindelusions.com>
2012-03-22 09:11:19 -0700

    BaseUI > Android Prefs and Keyboard/Input examples

---
+++ Resources/ui/handheld/android/baseui/preferences.js
@@ -0,0 +1,19 @@
+	// See:
+	//	platform/android/res/xml/preferences.xml
+	//	platform/android/res/values/array.xml
+	// Similar files MUST be present at build time or
+	// Titanium.UI.Android.openPreferences() will simply
+	// do nothing.
+function prefs() {
+	var win = Ti.UI.createWindow();	
+	var btn = Titanium.UI.createButton({
+		title:	'Click to Open Preferences'
+	});
+	btn.addEventListener('click', function() {
+		Titanium.UI.Android.openPreferences();
+	});
+	win.add(btn);
+	return win;
+};
+
+module.exports = prefs;
