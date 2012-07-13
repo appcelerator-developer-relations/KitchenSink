76ded72245b2a5570b6880670cd46c78f014ce03
David Lifshitz <dlifshitz@macadamian.com>
2012-07-13 14:46:05 -0400

    fix reading large app.js and remove extra bootstrap.js

--- build/blackberry/KitchenSink/main.cpp
+++ build/blackberry/KitchenSink/main.cpp
@@ -6,27 +6,24 @@
  */
 
 #include "tibb.h"
-#include <stdio.h>
-#include <malloc.h>
+
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 int main(int argc, char** argv)
 {
-    FILE* fp;
-    fp = fopen("app/native/assets/app.js", "r");
-    if (fp == NULL)
+    string javascript;
     {
-        return -1;
+        ifstream ifs("app/native/assets/app.js");
+        if (!ifs)
+        {
+            return -1;
+        }
+        getline(ifs, javascript, string::traits_type::to_char_type(string::traits_type::eof()));
+        ifs.close();
     }
-    char* buffer = (char*) malloc(4096);
-    size_t len = fread(buffer, 1, 4096, fp);
-    if (len < 1)
-    {
-        free(buffer);
-        return -1;
-    }
-    buffer[len] = 0;
-    fclose(fp);
-    int ret = tibb_run(buffer, argc, argv);
-    free(buffer);
+    int ret = tibb_run(javascript.c_str(), argc, argv);
     return ret;
 }
