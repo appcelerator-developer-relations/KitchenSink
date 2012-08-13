fa1c8767d8c99bae568b8fc9bb6d521011c15a32
Vishal Duggal <vishalduggal@gmail.com>
2012-08-13 13:10:52 -0700

    [TIMOB-10000] Remove non wprking remote image url

--- Resources/ui/common/baseui/image_view_positioning.js
+++ Resources/ui/common/baseui/image_view_positioning.js
@@ -2,7 +2,7 @@ function image_view_position() {
 	var win = Ti.UI.createWindow();
 	
 	var view = Ti.UI.createImageView({
-		image:'http://www.appcelerator.com/wp-content/uploads/2009/06/titanium_desk.png',
+		image:'http://static.appcelerator.com/images/header/appc_logo.png',
 		top:10,
 		left:10,
 		height:'auto',
