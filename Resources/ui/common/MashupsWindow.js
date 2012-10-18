a4c94f0e7d83a249809427d11caff0f256a1bf36
Ping Wang <pwang@appcelerator.com>
2012-10-17 17:50:34 -0700

    timob-11526: fixed text color issue.

--- Resources/ui/common/MashupsWindow.js
+++ Resources/ui/common/MashupsWindow.js
@@ -25,7 +25,14 @@ function MashupsWindow(title) {
 	data.push({title:'SOAP', hasChild:!isMobileWeb, test:'ui/common/mashups/soap', touchEnabled:!isMobileWeb, color:isMobileWeb?"#aaa":"#000"});
 	
 	// create table view
-	for (var i = 0; i < data.length; i++ ) { data[i].color = '#000'; data[i].font = {fontWeight:'bold'} };
+	for (var i = 0; i < data.length; i++ ) {
+		var d = data[i];
+		// On Android, if touchEnabled is not set explicitely, its value is undefined.
+		if (d.touchEnabled !== false) {
+			d.color = '#000';
+		}
+		d.font = {fontWeight:'bold'};
+	};
 	var tableview = Titanium.UI.createTableView({
 		data:data
 	});
--- Resources/ui/common/baseui/views.js
+++ Resources/ui/common/baseui/views.js
@@ -49,7 +49,14 @@ function views(_args) {
 	}
 	
 	// create table view
-	for (var i = 0; i < data.length; i++ ) { data[i].color = '#000'; data[i].font = {fontWeight:'bold'} };
+	for (var i = 0; i < data.length; i++ ) {
+		var d = data[i];
+		// On Android, if touchEnabled is not set explicitely, its value is undefined.
+		if (d.touchEnabled !== false) {
+			d.color = '#000';
+		}
+		d.font = {fontWeight:'bold'};
+	};
 	var tableview = Titanium.UI.createTableView({
 		data:data
 	});
