d67ba3197b7c72267fe78c1a0f522c37230e604d
Hieu Pham <hpham@appcelerator.com>
2012-12-12 16:34:57 -0800

    timob-11288: fix crashing issue

--- Resources/ui/handheld/android/baseui/android_menu_3.js
+++ Resources/ui/handheld/android/baseui/android_menu_3.js
@@ -1,9 +1,8 @@
 function android_menu3(_args) {
-	//var activity = Ti.Android.currentActivity;
-	var win = Ti.UI.currentWindow;
-	
-	win.backgroundColor = 'white';
-	win.title = _args.title;
+	var win = Ti.UI.createWindow({
+		title: _args.title,
+		backgroundColor: 'white'
+	});
 	
 	var b1 = Ti.UI.createButton({
 		title : 'Open Window',
