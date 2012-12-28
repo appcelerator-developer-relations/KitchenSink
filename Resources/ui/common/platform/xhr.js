acb1b412edb6cd8ca3c814a69ac68d01f22e5142
Ping Wang <pwang@appcelerator.com>
2012-12-28 10:31:33 -0800

    timob-11526: fixed font inconsistency

--- Resources/ui/common/platform/xhr.js
+++ Resources/ui/common/platform/xhr.js
@@ -1,5 +1,8 @@
 function xhr(_args) {
-	var self = Ti.UI.createWindow(),
+	var self = Ti.UI.createWindow({
+		title:_args.title,
+		backgroundColor:'#fff'
+	}),
 		
 		// if we're mobile web, don't make the rows touch enabled
 		isMobileWeb = Ti.Platform.osname == "mobileweb";
@@ -22,6 +25,7 @@ function xhr(_args) {
 		data.push({title:'File Upload', hasChild:true, test:'ui/handheld/ios/platform/xhr_fileupload'});
 	}
 	// create table view
+	for (var i = 0; i < data.length; i++ ) { data[i].color = '#000'; data[i].font = {fontWeight:'bold'} };
 	var tableview = Titanium.UI.createTableView({
 		data:data
 	});
