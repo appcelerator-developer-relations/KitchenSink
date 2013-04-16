365a156dcc30e8f04ceb8cfee1244b74291da509
Roman Kamenetsky <roman.kamenetskyi@globallogic.com>
2013-04-16 15:31:04 +0300

    Fix Platform->Tizen->Application test according to new callback implementation

--- Resources/ui/handheld/tizen/platform/application/appscontext.js
+++ Resources/ui/handheld/tizen/platform/application/appscontext.js
@@ -7,28 +7,32 @@ function tizen_appscontext(_args) {
 		helper = new (require('ui/handheld/tizen/platform/application/helper'));
 
 	// Return information available about a running application.
-	Tizen.Apps.getAppsContext(function(contexts) {
-		var data = [],
-			i = 0,
-			contextsCount = contexts.length,
-			tableview = Ti.UI.createTableView({});
+	Tizen.Apps.getAppsContext(function(response) {
+		if (response.success) {
+			var data = [],
+				i = 0,
+				contexts = response.contexts,
+				contextsCount = contexts.length,
+				tableview = Ti.UI.createTableView({});
 
-		// Generate app context info to show in the table view
-		for (; i < contextsCount; i++) {
-			data.push({
-				// AlertDialog title
-				title: contexts[i].appId + '\n<b>' + contexts[i].id + '</b>',
-				app_id: contexts[i].appId,
-				id: contexts[i].id
-			});
+			// Generate app context info to show in the table view
+			for (; i < contextsCount; i++) {
+				data.push({
+					// AlertDialog title
+					title: contexts[i].appId + '\n<b>' + contexts[i].id + '</b>',
+					app_id: contexts[i].appId,
+					id: contexts[i].id
+				});
+			}
+			tableview.data = data;
+
+			// Show dialog with Application information
+			tableview.addEventListener('click', helper.showAppInfoById);
+			self.add(tableview);
+		} else {
+			helper.showErrorDialog(response.error);
 		}
-		tableview.data = data;
-
-		// Show dialog with Application information
-		tableview.addEventListener('click', helper.showAppInfoById);
-		self.add(tableview);
-
-	}, helper.showErrorDialog);
+	});
 
 	return self;
 
