bdf8a313fe5be2ba8e579ec3efdab3e453494a2f
Vishal Duggal <vishalduggal@gmail.com>
2013-08-23 10:57:00 -0700

    UI Fix for IOS7

--- Resources/ui/common/baseui/window_standalone.js
+++ Resources/ui/common/baseui/window_standalone.js
@@ -327,6 +327,9 @@ function win_standalone(_args) {
 		top:310
 	});
 	
+	Ti.include("/etc/version.js");
+	var isIOS7 = isiOS7Plus();
+
 	b7.addEventListener('click', function()
 	{
 		var label = Titanium.UI.createButton({
@@ -356,11 +359,17 @@ function win_standalone(_args) {
 			Ti.API.info('IN HERE');
 			w.close();
 		});
-	
+		
+		if(isIOS7) {
+			theTop = 20;
+		} else {
+			theTop = 0;
+		}
+		
 		// create and add toolbar
 		var toolbar = Titanium.UI.iOS.createToolbar({
 			items:[hello,flexSpace,label, flexSpace,close],
-			top:0,
+			top:theTop,
 			borderTop:false,
 			borderBottom:true
 		});
@@ -375,7 +384,11 @@ function win_standalone(_args) {
 	
 		move.addEventListener('click', function()
 		{
-			toolbar.animate({top:20,duration:500});
+			if(isIOS7) {
+				toolbar.animate({top:40,duration:500});
+			} else {
+				toolbar.animate({top:20,duration:500});
+			}
 		});
 	
 		w.open();
