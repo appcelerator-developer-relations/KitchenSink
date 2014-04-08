b4bc6018fd4695c26049d072301c58e61f621ab3
pec1985 <pedro.tma@gmail.com>
2014-04-08 16:17:14 -0700

    [TIMOB-14021] Removed foursquare from Kitchen Sink

--- Resources/ui/common/MashupsWindow.js
+++ Resources/ui/common/MashupsWindow.js
@@ -10,7 +10,6 @@ function MashupsWindow(title) {
 	// create table view data object
 	var data = [
 		{title:'Twitter', hasChild:!isMobileWeb, test:'ui/common/mashups/twitter', title_image:'/images/twitter_logo_header.png', touchEnabled:!isMobileWeb, color:isMobileWeb?"#aaa":"#000"},
-		{title:'Foursquare', hasChild:!(isMobileWeb || isTizen), test:'ui/common/mashups/foursquare', title_image:'/images/light-poweredby-foursquare.png', touchEnabled:!(isMobileWeb || isTizen), color:isMobileWeb?"#aaa":"#000"},
 		{title:'Facebook', hasChild:!isTizen, test:'ui/common/mashups/facebook_test', touchEnabled: !isTizen},
 		//{title:'Dojo Mobile', hasChild:true, test:'ui/common/mashups/dojomobile'},
 		//{title:'Sencha Touch', hasChild:true, test:'ui/common/mashups/senchatouch'},
